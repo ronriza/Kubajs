3b450daf42bdbafad00f30a9ba2868521564a5cc
Ron Riza <ronriza91@gmail.com>
2021-08-17 06:01:36 -0400

    added minimax algorithm

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ function toArray() {
 let currentTurn = new Player('black');
 let currentOpp = new Player('white');
 let winner = null;
-let previousState = null;
 let currentState = toArray();
+let previousState = null;
 let moving = false;
 let selected = null;
 
@@ -35,69 +35,52 @@ let selected = null;
 // GAME LOGIC
 
 
-function getMarbleCount(color) {
-    // Accepts a color and returns the number of marbles of that color remaining on the board
-    let count = 0;
+function getMarbles(currentBoard, player) {
+    let playerMarbles = 0
     for (let i = 0; i < 7; i++) {
         for (let j = 0; j < 7; j++) {
-            if (document.querySelector(`.r${i}.c${j}`).firstElementChild.classList[1] === color) {
-                count++;
+            if (currentBoard[i][j] === player.color) {
+                playerMarbles++;
+                
             }
         }
     }
-    return count;
+    return playerMarbles
 }
 
-function checkWin(player, opponent) {
+function updateWinner(player, opponent) {
     // Checks if player has captured 7 marbles or knocked of all opponent marbles, 
     // or if the current turn has no remaining moves
     if (player.redMarbles === 7) {
         winner = { name: player.color, reason: `${player.color.charAt(0).toUpperCase() + player.color.slice(1)} collected 7 red marbles!` };
-    } else if (getMarbleCount(opponent.color) === 0) {
+    } else if (getMarbles(currentState, opponent) === 0) {
         winner = { name: player.color, reason: `No more ${currentOpp.color} marbles!` };
-    } else if (checkRemainingMoves(currentTurn) === false) {
+    } else if (checkRemainingMoves(currentTurn, currentOpp) === false) {
         winner = { name: currentOpp.color, reason: `${player.color.charAt(0).toUpperCase() + player.color.slice(1)} has no more moves` };
     }
 }
 
-async function cpuMove(player, opponent) {
-    // Searches through all available moves and picks a random one.
-    // If a move will result in a knockoff, it will pick that move
-    let moves = []
-    for (let i = 0; i < 7; i++) {
-        for (let j = 0; j < 7; j++) {
-            if (document.querySelector(`.r${i}.c${j}`).firstElementChild.classList[1] === 'white') {
-                let directions = ['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown'];
-                for (let direction of directions) {
-                    let result = fakeMove(player, i, j, direction)
-                    if (result.valid) {
-                        if (result.capturedRed || result.knockedOffOpp) {
-                            await makeMove(i, j, direction);
-                            checkWin(player, opponent);
-                            updateStats();
-                            return;
-                        }
-                        moves.push({ row: i, columm: j, direction: direction });
-                    }
-                }
-            }
-        }
+function checkIfWinner(availableMoves, opponentMarbles, player) {
+    if (availableMoves.length === 0) {
+        return true;
+    } else if (player.redMarbles === 7){
+        return true;
+    } else if (opponentMarbles ===0){
+        return true;
     }
-    let rand = Math.floor(Math.random() * moves.length);
-    let move = moves[rand];
-    await makeMove(move.row, move.columm, move.direction);
-    checkWin(player, opponent);
-    updateStats();
+    return false;
+    
 }
 
-function checkRemainingMoves(player) {
+
+function checkRemainingMoves(player, opponent) {
     // Returns true if the player has any moves screen. Otherwise returns false
     for (let i = 0; i < 7; i++) {
         for (let j = 0; j < 7; j++) {
             if (document.querySelector(`.r${i}.c${j}`).firstElementChild.classList[1] === player.color) {
                 let directions = ['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown'];
                 for (let direction of directions) {
-                    if (fakeMove(player, i, j, direction).valid) {
+                    if (fakeMove(currentState, previousState, player, opponent, i, j, direction).valid) {
                         return true;
                     }
                 }
@@ -106,6 +89,26 @@ function checkRemainingMoves(player) {
     }
     return false;
 }
+
+function getAllMoves(currentBoard, previousBoard, player, opponent){
+    const moves = []
+    for (let i = 0; i < 7; i++) {
+        for (let j = 0; j < 7; j++) {
+            if (currentBoard[i][j] === player.color) {
+                let directions = ['ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown'];
+                for (let direction of directions) {
+                    let results = fakeMove(currentBoard, previousBoard, player, opponent, i, j, direction)
+                    if (results.valid) {
+                        moves.push({row: i, column: j, direction: direction, results: results})
+                    }
+                }
+            }
+        }
+    }
+    return moves;
+}
+
+
 
 function updateStats() {
     // Updates the current turn and captured marbles on the UI. If a winner has been declared,
@@ -125,12 +128,13 @@ function updateStats() {
     document.querySelector(`.${currentOpp.color}captured`).innerHTML = currentOpp.redMarbles;
 }
 
-function fakeMove(player, row, column, direction, notify = false) {
+function fakeMove(currentBoard, previousBoard, player, opponent, row, column, direction, notify = false) {
     // Attempts to make move the piece at the given row and column in the given direction.
     // Returns an object with boolean properties reflecting if the move is valid, if it
     // will result in a red marble captured, or if it will result in an opponent marble knocked off
-    const result = { valid: true, capturedRed: false, knockedOffOpp: false };
-    const copy_board = JSON.parse(JSON.stringify(currentState));
+    const copyBoard = JSON.parse(JSON.stringify(currentBoard));
+    const copyPrevious = JSON.parse(JSON.stringify(previousBoard));
+    const result = { valid: true, capturedRed: false, knockedOffOpp: false, newBoard: null};
     let rowModifier = null;
     let columnModifier = null;
     let edgeRow = row;
@@ -140,13 +144,13 @@ function fakeMove(player, row, column, direction, notify = false) {
         rowModifier = 0
         columnModifier = -1
         edgeColumn = 0
-        if (column !== 6 && copy_board[row][column + 1] !== 'empty') {
+        if (column !== 6 && copyBoard[row][column + 1] !== 'empty') {
             if (notify === true) alert('There is no space behind the marble');
             result.valid = false
             return result;
         }
         for (let i = 0; i < column; i++) {
-            if (copy_board[row][i] === 'empty') {
+            if (copyBoard[row][i] === 'empty') {
                 containsEmpty = true;
             }
         }
@@ -154,13 +158,13 @@ function fakeMove(player, row, column, direction, notify = false) {
         rowModifier = 0
         columnModifier = 1
         edgeColumn = 6
-        if (column !== 0 && copy_board[row][column - 1] !== 'empty') {
+        if (column !== 0 && copyBoard[row][column - 1] !== 'empty') {
             if (notify === true) alert('There is no space behind the marble');
             result.valid = false
             return result;
         }
         for (let i = column + 1; i < 7; i++) {
-            if (copy_board[row][i] === 'empty') {
+            if (copyBoard[row][i] === 'empty') {
                 containsEmpty = true;
             }
         }
@@ -168,13 +172,13 @@ function fakeMove(player, row, column, direction, notify = false) {
         rowModifier = -1
         columnModifier = 0
         edgeRow = 0
-        if (row !== 6 && copy_board[row + 1][column] !== 'empty') {
+        if (row !== 6 && copyBoard[row + 1][column] !== 'empty') {
             if (notify === true) alert('There is no space behind the marble');
             result.valid = false
             return result;
         }
         for (let i = 0; i < row; i++) {
-            if (copy_board[i][column] === 'empty') {
+            if (copyBoard[i][column] === 'empty') {
                 containsEmpty = true;
             }
         }
@@ -182,44 +186,45 @@ function fakeMove(player, row, column, direction, notify = false) {
         rowModifier = 1
         columnModifier = 0
         edgeRow = 6
-        if (row !== 0 && copy_board[row - 1][column] !== 'empty') {
+        if (row !== 0 && copyBoard[row - 1][column] !== 'empty') {
             if (notify === true) alert('There is no space behind the marble');
             result.valid = false
             return result;
         }
         for (let i = row + 1; i < 7; i++) {
-            if (copy_board[i][column] === 'empty') {
+            if (copyBoard[i][column] === 'empty') {
                 containsEmpty = true;
             }
         }
     }
 
     if (containsEmpty === false) {
-        if (copy_board[edgeRow][edgeColumn] === player.color) {
+        if (copyBoard[edgeRow][edgeColumn] === player.color) {
             if (notify === true) alert('You cannot knock of your own marble');
             result.valid = false;
             return result;
-        } else if (copy_board[edgeRow][edgeColumn] === 'red') {
+        } else if (copyBoard[edgeRow][edgeColumn] === 'red') {
             result.capturedRed = true;
-        } else if (copy_board[edgeRow][edgeColumn] === currentOpp.color) {
+        } else if (copyBoard[edgeRow][edgeColumn] === opponent.color) {
             result.knockedOffOpp = true;
         }
-        copy_board[edgeRow][edgeColumn] = 'empty';
+        copyBoard[edgeRow][edgeColumn] = 'empty';
     }
     let prev = 'empty';
     do {
-        let current = copy_board[row][column];
-        copy_board[row][column] = prev;
+        let current = copyBoard[row][column];
+        copyBoard[row][column] = prev;
         prev = current;
         column += columnModifier;
         row += rowModifier
     } while (prev !== 'empty');
 
-    if (JSON.stringify(copy_board) === JSON.stringify(previousState)) {
+    if (JSON.stringify(copyBoard) === JSON.stringify(copyPrevious)) {
         if (notify === true) alert('You cannot return to the previous board position');
         result.valid = false
         return result;
     }
+    result.newBoard = copyBoard
     return result;
 }
 
@@ -288,12 +293,16 @@ async function makeMove(row, column, direction) {
         current = document.querySelector(`.r${tempRow}.c${tempCol}`).firstElementChild.classList[1];
         if (current !== 'empty') {
             document.querySelector(`.r${tempRow}.c${tempCol}`).firstElementChild.classList.add(movingClass);
+
         }
         tempCol += columnModifier;
         tempRow += rowModifier;
     }
     while (current !== 'empty');
+
+
     await new Promise(resolve => setTimeout(resolve, 500));
+
     let prev = 'empty';
     do {
         let current = document.querySelector(`.r${row}.c${column}`).firstElementChild.classList[1];
@@ -318,26 +327,31 @@ async function makeMove(row, column, direction) {
 
 
 async function checkMove(direction, square) {
-    // Checks if a move is valid, executes the move, and then makes CPU moves (as necessary)
     let player = currentTurn;
     let opponent = currentOpp;
-    let color = square.firstElementChild.classList[1];
     let row = parseInt(square.classList[0][1]);
     let column = parseInt(square.classList[1][1]);
-    let validMove = fakeMove(player, row, column, direction, true)
+    let validMove = fakeMove(currentState, previousState, player, opponent, row, column, direction, true)
     if (validMove.valid === false) {
         moving = false;
         return;
     }
     await makeMove(row, column, direction);
-    checkWin(player, opponent);
+    updateWinner(player, opponent);
     updateStats();
     while (!winner && currentTurn.color === 'white') {
-        await new Promise(resolve => setTimeout(resolve, 500));
-        await cpuMove(currentTurn, currentOpp);
+        await new Promise(resolve => setTimeout(resolve, 1));
+        await cpuMove(currentState, previousState, currentTurn, currentOpp);
     }
     moving = false;
 
+}
+
+async function cpuMove(currentState, previousState, currentTurn, currentOpp){
+    let bestMove = aiMove(currentState, previousState, currentTurn, currentOpp)
+    await makeMove(bestMove.row, bestMove.column, bestMove.direction)
+    updateWinner(currentTurn, currentOpp);
+    updateStats();
 }
 
 // EVENT LISTENERS
@@ -407,4 +421,108 @@ for (const rulesToggle of rulesToggles) {
         document.querySelector('.rules').classList.toggle('hidden');
     })
 }
+
+
+
+function aiMove(currentState, previousState, currentTurn, currentOpp) {
+    let aiMarbles = getMarbles(currentState, currentTurn)
+    let humanMarbles = getMarbles(currentState, currentOpp)
+    let startingAiScore = getScore(currentTurn, currentOpp, aiMarbles, humanMarbles)
+    let startingHumanScore = getScore(currentOpp, currentTurn, humanMarbles, aiMarbles)
+    return minimax(currentState, previousState, currentTurn, currentOpp, startingAiScore, startingHumanScore, 3)
+
+}
+
+
+function minimax(currentBoard, previousBoard, player, opponent, startingPlayerScore, startingOpponentScore, depth) {
+
+    const availableMoves = getAllMoves(currentBoard, previousBoard, player, opponent);
+    const opponentMarbles = getMarbles(currentBoard, opponent)
+    if (checkIfWinner(availableMoves, opponentMarbles, player)){
+        if (player.color === 'black'){
+            return {score: -100}
+        } else {
+            return {score: 100}
+        }
+    } else if (depth === 0) {
+        const playerMarbles = getMarbles(currentBoard, player)
+        let playerScore = getScore(player, opponent, playerMarbles, opponentMarbles)
+        let opponentScore = getScore(opponent, player, opponentMarbles, playerMarbles)
+        if (player.color === 'black'){
+            return {score: -((playerScore - startingPlayerScore) - (opponentScore - startingOpponentScore))}
+        } else {
+            return {score: (playerScore - startingPlayerScore) - (opponentScore - startingOpponentScore)}
+        }
+        
+    }
+
+    const testMoves = []
+    const oldBoard = JSON.parse(JSON.stringify(currentBoard));
+    for (let i=0; i<availableMoves.length; i++){
+        let moveInfo = {}
+        moveInfo.row = availableMoves[i].row
+        moveInfo.column = availableMoves[i].column
+        moveInfo.direction = availableMoves[i].direction
+        let fakeResults = availableMoves[i].results
+        let newPlayer = new Player(player.color)
+        let newOpp = new Player(opponent.color)
+        newPlayer.redMarbles = player.redMarbles
+        newOpp.redMarbles = opponent.redMarbles
+
+        if (fakeResults.capturedRed) {
+            newPlayer.redMarbles++;
+        }
+
+        if (fakeResults.knockedOffOpp || fakeResults.capturedRed) {
+            const result = minimax(fakeResults.newBoard, oldBoard, newPlayer, newOpp, startingPlayerScore, startingOpponentScore, depth-1)
+            moveInfo.score = result.score
+        } else {
+            const result = minimax(fakeResults.newBoard, oldBoard, newOpp, newPlayer, startingOpponentScore, startingPlayerScore, depth-1)
+            moveInfo.score = result.score
+        }
+        testMoves.push(moveInfo)
+    }
+
+    let bestMove = null;
+
+    console.log(testMoves)
+
+    if (testMoves.every((val, i, arr) => val.score === arr[0].score)){
+        let rand = Math.floor(Math.random() * testMoves.length);
+        return testMoves[rand]
+    }
+
+    if (player.color === 'white') {
+        let bestScore = -Infinity
+        for (let i=0; i< testMoves.length; i++){
+            if (testMoves[i].score > bestScore){
+                bestScore = testMoves[i].score
+                bestMove = i
+            }
+        }
+    } else {
+        let bestScore = Infinity
+        for (let i=0; i< testMoves.length; i++){
+            if (testMoves[i].score < bestScore){
+                bestScore = testMoves[i].score
+                bestMove = i
+            }
+        }
+
+    }
+    return testMoves[bestMove]
+}
+
+
+
+function getScore(player, opponent, playerMarbles, opponentMarbles) {
+    let playerKnockoffs = 8 - opponentMarbles + player.redMarbles
+    let opponentKnockoffs = 8 - playerMarbles + opponent.redMarbles 
+    return playerKnockoffs - opponentKnockoffs
+}
+
+
+
+
+
 
